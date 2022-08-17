a0b226ec19f28ab631bd6211d789161800edb59c
Matias Ruiz Silva <ruizsilvamatias@gmail.com>
2022-08-17 18:25:43 -0300

    first commit del programa

---
+++ README.md
@@ -0,0 +1,5 @@
+# Encriptador-
+
+Primer Challenge de Alura latam y oracle.
+
+Coinciste en un programa para encriptar un texto y hacerlo ilegible, luego con otro boton se puede desifran el mensaje que guarda.-
---
+++ encriptador.js
@@ -0,0 +1,25 @@
+function encriptar(texto) {
+    for (let letra of texto) {
+        if (letra == "a") {
+            letra = "ai";
+        } else if (letra == "e") {
+            letra = "enter";
+        } else if (letra == "i") {
+            letra = "imes";
+        } else if (letra == "o") {
+            letra = "ober";
+        } else if (letra == "u") {
+            letra = "ufat";
+        }
+    }
+}
+
+// La letra "a" es convertida para "ai"
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
